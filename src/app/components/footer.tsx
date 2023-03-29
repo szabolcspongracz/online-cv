43da2b1e06139fdfc638050762f4f78a7516ffb5
Szabolcs Pongrácz <pongraczsz@abylon.hu>
2023-03-29 11:03:46 +0200

    basic layout modifications

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => (
+  <>
+    <footer>
+      <p>Test footer</p>
+    </footer>
+  </>
+);
---
+++ src/app/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => (
+  <>
+    <header>
+      <p>Test Header</p>
+    </header>
+  </>
+);
